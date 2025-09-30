a372f1a053624928a5b910897be51bd939d2dccf
Aidan <aidan.cheng94@gmail.com>
2025-09-30 15:16:48 +0100

    Fixing unsued icons

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,7 +2,6 @@ import { ReactNode, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { 
   HomeIcon, 
-  ChartBarIcon,
   UserGroupIcon,
   Bars3Icon,
   XMarkIcon,
